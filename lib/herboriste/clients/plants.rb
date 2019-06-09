92b8fa95530e86938c3adac2ed47d4189a7f965d
Elliott Hébert <ell.heb@container4.ch>
2019-06-09 20:38:26 +0200

    parse answer body

--- lib/herboriste/clients/plants.rb
+++ lib/herboriste/clients/plants.rb
@@ -5,8 +5,14 @@ module Herboriste
         raise ArgumentError, "You at least need to pass a query or an id" unless params.has_key? :query or params.has_key? :id
         q = "?q=#{params[:query]}" if params.has_key? :query
         q = "/#{params[:id]}" if params.has_key? :id
-        puts "/plants" + q
-        return self.get("/plants" + q)
+        answer = self.get("/plants" + q)
+        return parse_body(answer[:body])
+      end
+
+      private
+
+      def parse_body(raw_body)
+        JSON.parse(raw_body)
       end
     end
   end
