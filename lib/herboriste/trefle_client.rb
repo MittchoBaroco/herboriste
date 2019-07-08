033d4e027578f410118dcf1a3f87ff87b3a67d78
mug4z <mug4z@protonmail.com>
2019-07-08 12:09:47 +0200

    Ajout du get_species

--- lib/herboriste/trefle_client.rb
+++ lib/herboriste/trefle_client.rb
@@ -1,11 +1,12 @@
 require 'herboriste/connection'
 require 'herboriste/clients/plants'
+require 'herboriste/clients/species'
 
 module Herboriste
   class TrefleClient
     include Herboriste::Connection
     include Herboriste::TrefleClient::Plants
-
+    include Herboriste::TrefleClient::Species
     def initialize(params={token: ENV['TREFLE_TOKEN']})
       raise ArgumentError, "Token can't be blank" if params[:token].nil? or params[:token].empty?
 
